637e74cd0dc955ff9e0b47db66830287996156af
Daichi-appy <daichi.es47@gmail.com>
2023-10-03 20:47:27 +0900

    validation追加

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"tech-shelf/repository"
 	"tech-shelf/router"
 	"tech-shelf/usecase"
+	"tech-shelf/validator"
 )
 
 func main() {
 	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	taskValidator := validator.NewTaskValidator()
 	userRepository := repository.NewUserRepository(db)
 	taskRepository := repository.NewTaskRepository(db)
-	userUsecase := usecase.NewUserUsecase(userRepository)
-	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
 	userController := controller.NewUserController(userUsecase)
 	taskController := controller.NewTaskController(taskUsecase)
 	e := router.NewRouter(userController, taskController)
---
+++ validator/user_validator.go
@@ -0,0 +1,34 @@
+package validator
+
+import (
+	"tech-shelf/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type IUserValidator interface {
+	UserValidate(user model.User) error
+}
+
+type UserValidator struct{}
+
+func NewUserValidator() IUserValidator {
+	return &UserValidator{}
+}
+
+func (tv *UserValidator) UserValidate(user model.User) error {
+	return validation.ValidateStruct(&user,
+		validation.Field(
+			&user.Email,
+			validation.Required.Error("email is required"),
+			validation.RuneLength(1, 30).Error("limited max 30 characters"),
+			is.Email.Error("invalid email format"),
+		),
+		validation.Field(
+			&user.Password,
+			validation.Required.Error("password is required"),
+			validation.RuneLength(6, 30).Error("limited min 6 max 30 characters"),
+		),
+	)
+}
